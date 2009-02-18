7f263d6eb9a6afe70a0ab0117d41751f317a3d20
Gareth Andrew <gingerhendrix@hotmail.com>
2009-02-18 16:39:31 +0000

    Adding untested artist.tags method

--- lib/scrobbler2/artist.rb
+++ lib/scrobbler2/artist.rb
@@ -15,5 +15,11 @@ module Scrobbler2
     get_resource :top_tags, :root => 'toptags'
     get_resource :top_tracks, :root => 'toptracks'
     
+    def tags
+      return @tags if @tags
+      @tags = self.class.get_with_auth("artist.gettags")["lfm"]["tags"]    
+    end
+
+    
   end
 end
